8157512eed0467c24e0a05eb1bee1f9289b2f16a
Prasanna Kanvinde <prassy@gmail.com>
2019-04-09 18:36:33 +0530

    added spring-boot-lobs project

---
+++ spring-boot-lobs/src/test/java/com/logixal/lobs/LobsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.logixal.lobs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LobsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
